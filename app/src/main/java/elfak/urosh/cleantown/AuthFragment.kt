aae38852f3c2fba3280627a305a755c29fb594cc
Urosh <urosh_h@protonmail.com>
2023-09-13 17:27:05 +0200

    auth fragment & viewmodel update & minor config changes

--- app/src/main/java/elfak/urosh/cleantown/AuthFragment.kt
+++ app/src/main/java/elfak/urosh/cleantown/AuthFragment.kt
@@ -1,7 +1,14 @@
 package elfak.urosh.cleantown
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.icu.text.SimpleDateFormat
+import android.net.Uri
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +16,13 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.FileProvider
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigation
+import java.io.File
+import java.util.*
+import androidx.fragment.app.activityViewModels
 
 class AuthFragment : Fragment() {
 
@@ -16,25 +30,20 @@ class AuthFragment : Fragment() {
         fun newInstance() = AuthFragment()
     }
 
-    private lateinit var viewModel: AuthViewModel
+    private val viewModel: AuthViewModel by activityViewModels()
+    private val IMAGE_PICK_REQUEST_CODE = 666
+    private lateinit var image: Uri
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        viewModel = ViewModelProvider(this).get(AuthViewModel::class.java)
         val view = inflater.inflate(R.layout.fragment_auth, container, false)
         viewModel.authState.observe(viewLifecycleOwner) { state ->
             when (state) {
                 AuthState.Success -> {
-                    Toast.makeText(activity, "Auth Success!", Toast.LENGTH_SHORT).show()
-                    val fragmentManager  = requireActivity().supportFragmentManager
-                    val transaction  = fragmentManager.beginTransaction()
-
-                    val newFragment = MapsFragment()
-                    transaction.replace(R.id.fragmentContainer, newFragment)
-                    transaction.commit()
-
+                    val options: NavOptions = NavOptions.Builder().setPopUpTo(R.id.authFragment, true).build()
+                    Navigation.findNavController(view).navigate(R.id.action_fragmentAuth_to_fragmentMaps, null, options)
                 }
                 AuthState.Failure -> {
                     Toast.makeText(activity, "Invalid Credentials", Toast.LENGTH_LONG).show()
@@ -46,24 +55,61 @@ class AuthFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = ViewModelProvider(this).get(AuthViewModel::class.java)
         val btnRegister = view.findViewById<Button>(R.id.btnRegister)
+        val btnLogin = view.findViewById<Button>(R.id.btnLogin)
+        val btnPhoto = view.findViewById<Button>(R.id.btnPhoto)
         val email = view.findViewById<EditText>(R.id.editTextEmail)
         val password = view.findViewById<EditText>(R.id.editTextPassword)
         val phone = view.findViewById<EditText>(R.id.editTextPhone)
+        btnPhoto.setOnClickListener {
+            val permissions = arrayOf(
+                Manifest.permission.CAMERA,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+
+            ActivityCompat.requestPermissions(requireActivity(), permissions, 666)
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            val photoFile: File? = createImageFile()
+
+            if (photoFile != null) {
+                image = FileProvider.getUriForFile(
+                    requireContext(),
+                    requireContext().packageName + ".provider",
+                    photoFile
+                )
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, image)
+                startActivityForResult(intent, IMAGE_PICK_REQUEST_CODE)
+            }
+        }
         btnRegister.setOnClickListener {
-            val result = viewModel.register(email.text.toString(), password.text.toString())
-            if(result != null) {
+            val email = email.text.toString() + "@cleantown.net"
+            val result =
+                viewModel.register(email, password.text.toString(), image, phone.text.toString())
+            if (result != null) {
                 Toast.makeText(activity, result.cause.toString(), Toast.LENGTH_LONG).show()
                 Toast.makeText(activity, result.message.toString(), Toast.LENGTH_LONG).show()
             }
         }
+        btnLogin.setOnClickListener {
+            val email = email.text.toString() + "@cleantown.net"
+            viewModel.login(email, password.text.toString())
+        }
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(AuthViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Toast.makeText(activity, "ONACTIVIRYRESULT", Toast.LENGTH_LONG).show()
+        if (requestCode == IMAGE_PICK_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+        }
+    }
+
+    private fun createImageFile(): File? {
+        // Create a unique image file name
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+        val storageDir = requireContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val imageFileName = "JPEG_${timeStamp}_"
+        return File.createTempFile(imageFileName, ".jpg", storageDir)
     }
 
 }
--- app/src/main/java/elfak/urosh/cleantown/AuthViewModel.kt
+++ app/src/main/java/elfak/urosh/cleantown/AuthViewModel.kt
@@ -1,10 +1,15 @@
 package elfak.urosh.cleantown
 
+import android.net.Uri
+import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.coroutines.tasks.await
 import kotlin.Exception
 
 enum class AuthState {
@@ -15,6 +20,9 @@ enum class AuthState {
 class AuthViewModel : ViewModel() {
     private val _liveAuthState = MutableLiveData<AuthState>()
     private val auth = FirebaseAuth.getInstance()
+    private val storage = FirebaseStorage.getInstance("gs://cleantown-4d5da.appspot.com")
+    private val db =
+        FirebaseDatabase.getInstance("https://cleantown-4d5da-default-rtdb.europe-west1.firebasedatabase.app/")
     val authState: LiveData<AuthState> = _liveAuthState
 
     fun isAuthenticated(): Boolean {
@@ -26,27 +34,52 @@ class AuthViewModel : ViewModel() {
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
                     _liveAuthState.value = AuthState.Success
-                }
-                else {
+                } else {
                     _liveAuthState.value = AuthState.Failure
                 }
             }
     }
 
-    fun register(email: String, password: String): Exception? {
+    fun register(email: String, password: String, img: Uri?, phone: String): Exception? {
         var exception: Exception? = null
+        if (img == null) {
+            return Exception("banana")
+        }
+        val storageRef = storage.reference
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
+                    val userId = auth.currentUser!!.uid.toString()
+                    storageRef.child("profile_images").child(userId).putFile(img)
+                        .addOnSuccessListener { task ->
+                            task.storage.downloadUrl.addOnSuccessListener { uri ->
+                                var user = HashMap<String, Any>()
+                                user["username"] = email.split('@')[0]
+                                user["profilePhoto"] = uri.toString()
+                                user["phone"] = phone
+                                user["userId"] = userId
+                                user["postedPoints"] = 0
+                                user["eventPoints"] = 0
+                                db.reference.child(userId).setValue(user)
+                                    .addOnFailureListener { ex ->
+                                        Log.d(
+                                            "DB",
+                                            ex.message.toString()
+                                        )
+                                    }
+                            }
+                        }
+                        .addOnFailureListener { ex ->
+                            Log.d("STORAGE", ex.message.toString())
+                        }
                     _liveAuthState.value = AuthState.Success
-                }
-                else {
+                } else {
                     _liveAuthState.value = AuthState.Failure
                 }
             }
             .addOnFailureListener { ex ->
                 exception = ex
-             }
-             return exception
+            }
+        return exception
     }
 }
